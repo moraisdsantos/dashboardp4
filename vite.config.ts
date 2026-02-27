c4dfe70ec8c34fd4a6a343b60889e19cb54ab11e
moraisdsantos <moraisdsantos@gmail.com>
2026-02-27 19:43:12 -0300

    Update repository name in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // GitHub Pages de repo = https://usuario.github.io/NOME-DO-REPO/
-const repo = 'p4dashboard'
+const repo = 'dashboardp4'
 
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? `/${repo}/` : '/',
